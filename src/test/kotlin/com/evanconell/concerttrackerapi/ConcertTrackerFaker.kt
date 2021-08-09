ee955a1599dfa9c6c1cc402865943ee9d090cbee
personal <econell92@gmail.com>
2021-08-09 15:02:40 -0400

    CT-5: Add `ValidationResult.isValid` tests

--- src/test/kotlin/com/evanconell/concerttrackerapi/ConcertTrackerFaker.kt
+++ src/test/kotlin/com/evanconell/concerttrackerapi/ConcertTrackerFaker.kt
@@ -2,6 +2,7 @@ package com.evanconell.concerttrackerapi
 
 import com.evanconell.concerttrackerapi.model.command.CreateArtistCommand
 import com.evanconell.concerttrackerapi.model.data.Artist
+import com.evanconell.concerttrackerapi.model.validation.ValidationError
 import com.github.javafaker.Faker
 import org.bson.types.ObjectId
 
@@ -19,4 +20,10 @@ object ConcertTrackerFaker : Faker() {
     )
 
     fun ctCreateArtistCommand(): CreateArtistCommand = CreateArtistCommand(name = artist().name())
+
+    fun ctValidationError(): ValidationError = ValidationError(
+        field = name().firstName(),
+        value = name().lastName(),
+        message = shakespeare().hamletQuote()
+    )
 }
