48ae1e3f05f84d201ee6410f481acc73725b27cc
personal <econell92@gmail.com>
2021-08-10 23:00:48 -0400

    CT-12: Explicitly allow all origins for CORS

--- src/main/kotlin/com/evanconell/concerttrackerapi/controller/ArtistController.kt
+++ src/main/kotlin/com/evanconell/concerttrackerapi/controller/ArtistController.kt
@@ -12,8 +12,8 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
-
 @RestController
+@CrossOrigin(originPatterns = ["*"])
 class ArtistController(private val artistService: ArtistService) {
 
     @GetMapping("/artist")
